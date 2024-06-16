30b0363f6c77a314360c6cd1a9c79653515ecda1
CryptocoderAS <as7663@srmist.edu.in>
2024-06-16 15:20:36 +0530

    :zap: Intial

--- scripts.js
+++ scripts.js
@@ -1,19 +1,20 @@
 async function login() {
   const username = document.getElementById("loginUsername").value;
   const password = document.getElementById("loginPassword").value;
+  const apiUrl = process.env.REACT_APP_API_URL;
 
   try {
-      const response = await fetch('https://password1vault.vercel.app/auth/login', {
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ username, password })
-      });
+    const response = await fetch(`${apiUrl}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    });
 
-      if (!response.ok) {
-          throw new Error('Login failed');
-      }
+    if (!response.ok) {
+      throw new Error('Login failed');
+    }
 
       const data = await response.json();
       const token = data.token;
@@ -32,9 +33,10 @@ async function login() {
 async function signup() {
   const username = document.getElementById("signupUsername").value;
   const password = document.getElementById("signupPassword").value;
+  const apiUrl = process.env.REACT_APP_API_URL;
 
   try {
-      const response = await fetch('https://password1vault.vercel.app/auth/signup', {
+    const response = await fetch(`${apiUrl}/auth/signup`, {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json'
@@ -83,9 +85,10 @@ document.getElementById("toggleThemeButton").addEventListener("click", toggleThe
 async function login() {
   const username = document.getElementById("loginUsername").value;
   const password = document.getElementById("loginPassword").value;
+  const apiUrl = process.env.REACT_APP_API_URL;
 
   try {
-      const response = await fetch('https://password1vault.vercel.app/auth/login', {
+      const response = await fetch(`${apiUrl}/auth/login`, {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json'
@@ -115,9 +118,10 @@ async function login() {
 async function signup() {
   const username = document.getElementById("signupUsername").value;
   const password = document.getElementById("signupPassword").value;
+  const apiUrl = process.env.REACT_APP_API_URL;
 
   try {
-      const response = await fetch('https://password1vault.vercel.app/auth/signup', {
+      const response = await fetch(`${apiUrl}/auth/signup`, {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json'
@@ -166,9 +170,11 @@ document.getElementById("toggleThemeButton").addEventListener("click", toggleThe
 
 function checkLoggedIn() {
   const token = localStorage.getItem('token');
+  const apiUrl = process.env.REACT_APP_API_URL;
+
   if (token) {
      
-      fetch('https://password1vault.vercel.app/auth/validate', {
+      fetch( `${apiUrl}/auth/validate`, {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
